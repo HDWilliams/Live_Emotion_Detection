fa650734d33481c6cdc009e1d1c45a7b6aed3141
HDWilliams <hughdaywilliams@gmail.com>
2021-01-18 22:13:55 -0500

    started basic flask server

--- server.py
+++ server.py
@@ -1,7 +1,19 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/test', methods=['POST'])
+def test():
+    if request.method == 'POST':
+        for f in request.files:
+            print(f)
+    return
+
+
 @app.route('/predict', methods=['POST'])
 def img_predict():
-    
+    return None
